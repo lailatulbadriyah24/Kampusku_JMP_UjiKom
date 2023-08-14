8633c3d07b1b947953003990796e3a785fc334b5
lailatulbadriyah24 <lailatulbadriyah654@gmail.com>
2023-08-14 08:57:30 +0700

    SplashScreen, Register, Login

---
+++ app/src/main/java/com/example/kampusku/RegisterActivity.java
@@ -0,0 +1,80 @@
+package com.example.kampusku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText TxEmail, TxPassword, TxConPassword;
+    Button BtnRegister;
+    DBHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        dbHelper = new DBHelper(this);
+
+        TxEmail = (EditText)findViewById(R.id.txEmailReg);
+        TxPassword = (EditText)findViewById(R.id.txPasswordReg);
+        TxConPassword = (EditText)findViewById(R.id.txConPassword);
+        BtnRegister = (Button)findViewById(R.id.btnRegister);
+
+        TextView tvRegister = (TextView)findViewById(R.id.tvRegister);
+
+        tvRegister.setText(fromHtml("Back to " + "</font><font color='#3b5998'>Login</font>"));
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        BtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = TxEmail.getText().toString().trim();
+                String password = TxPassword.getText().toString().trim();
+                String conPassword = TxConPassword.getText().toString().trim();
+
+                ContentValues values = new ContentValues();
+
+
+                if (!password.equals(conPassword)){
+                    Toast.makeText(RegisterActivity.this, "Password not match", Toast.LENGTH_SHORT).show();
+                }else if (password.equals("") || email.equals("")){
+                    Toast.makeText(RegisterActivity.this, "Email or Password cannot be empty", Toast.LENGTH_SHORT).show();
+                }else {
+                    values.put(DBHelper.row_email, email);
+                    values.put(DBHelper.row_password, password);
+                    dbHelper.insertData(values);
+
+                    Toast.makeText(RegisterActivity.this, "Register successful", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        });
+    }
+
+    public static Spanned fromHtml(String html){
+        Spanned result;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            result = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
+        }else {
+            result = Html.fromHtml(html);
+        }
+        return result;
+    }
+}
---
+++ app/src/main/java/com/example/kampusku/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.example.kampusku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        }, 5000L); //5000 L = 5 detik
+    }
+}
